bf73ec16c516c513cebca7a5cbcb7f4d84c1faf1
katherine <116115224+kaerulean@users.noreply.github.com>
2023-10-16 12:39:42 -0400

    asdas\

--- main.js
+++ main.js
@@ -2,5 +2,16 @@ DOMSelectors = {
     form: document.querySelector("#form"),
 }
 
-console.log(DOMSelectors.form);
+/* DOMSelectors = {
+    //stores all related code for selecting elements here
+        form: document.querySelector("#form"),
+        firstName: document.querySelector(".first-name"),
+        h2s: document.querySelectorAll("h2"), //querySelector only grabs the first h2, querySelectorAll grabs all
+    };
+    
+    DOMSelectors.form.addEventListener("submit", function(event) {
+        event.preventDefault(); //prevents refresh
+        DOMSelectors.h2s.forEach((header)=> header.textContent = DOMSelectors.firstName.value); //h2s<- multiple h2
+    });
 
+ */
