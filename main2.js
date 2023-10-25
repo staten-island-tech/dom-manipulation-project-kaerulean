a50cf47167a8a4cec108dffc4ffff234414ea1fb
katherine <116115224+kaerulean@users.noreply.github.com>
2023-10-25 18:25:20 -0400

    HAHA LOSERRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR

--- main2.js
+++ main2.js
@@ -3,12 +3,12 @@ const DOMSelectors = {
     button: document.querySelector("#button"),
     container: document.querySelector(".container"),
     remove: document.querySelector("#remove"),
-    box: document.querySelectorAll(".box"),
     inputRed: document.querySelector("#form-red"), //make separate inputs for each?!
     inputBlue: document.querySelector("#form-blue"),
     inputGreen: document.querySelector("#form-green"),
-    h3s: document.querySelectorAll("h3")
+    h3s: document.querySelectorAll("h3") //i dont even why this is here but i'm keeping it for funsies
 }
+
 
 function click(){
     DOMSelectors.button.addEventListener("click", function(event){
@@ -21,25 +21,38 @@ function click(){
         const userInputBlue = DOMSelectors.inputBlue.value; 
         console.log(userInputBlue);
         
-        //DOMSelectors.box.style.backgroundColor = 'rgb(' + userInputRed + ',' + userInputGreen + ',' + userInputBlue + ')'; 
-    
-        DOMSelectors.container.insertAdjacentHTML("beforeend", `<div class = "box"></div>`)
-    
-        event.preventDefault();
         
-       
+        if (Number(userInputRed) <= 255 && Number(userInputGreen) <= 255 && Number(userInputBlue) <= 255) { //alerts when user enters not valid rgb values
+            console.log("yay")
+          } else {
+            alert("hey those aren't valid rgb values")
+            return click()
+          }
 
+        const style = 'rgb(' + userInputRed + ',' + userInputGreen + ',' + userInputBlue + ')'; 
+
+        
+        const newBox = `<div class="box" style="background-color: ${style}"></div>`
+                 
+
+        DOMSelectors.container.insertAdjacentHTML("beforeend", `${newBox}`)
+        form.reset();
+        event.preventDefault();
     })
        
 }
 
 
 function remove(){
-    DOMSelectors.box.addEventListener("click", function() {
-        
+    DOMSelectors.container.addEventListener("click", function (event) {
+        if (event.target.classList.contains("box")) {
+            DOMSelectors.container.removeChild(event.target);
+            
+        }
     });
+    
 }
 
-
 click();
+
 remove();
